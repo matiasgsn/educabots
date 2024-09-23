9c0fd5dd4751b00c723c0c4c92dd4ffc389a35df
Matias <matiasgsn@gmail.com>
2024-09-23 02:23:14 -0300

    Adição de variáveis novas pra filtros, mudança na cor do botão de troca de modo

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let modeimg = document.querySelector('.modeselectimg')
 function swapMode(){
     if (currentmode == 0) {
         document.documentElement.style.setProperty('--main-color', 'var(--dark-color)');
+        document.documentElement.style.setProperty('--opposite-color', 'var(--light-color)');
+        document.documentElement.style.setProperty('--main-color-filter', 'var(--dark-color-filter)');
+        document.documentElement.style.setProperty('--opposite-color-filter', 'var(--light-color-filter)');
+
         modeimg.setAttribute('src', 'lightmode.svg');
         currentmode = 1;
     } else {
         document.documentElement.style.setProperty('--main-color', 'var(--light-color)');
+        document.documentElement.style.setProperty('--opposite-color', 'var(--dark-color)');
+        document.documentElement.style.setProperty('--main-color-filter', 'var(--light-color-filter)');
+        document.documentElement.style.setProperty('--opposite-color-filter', 'var(--dark-color-filter)');
+
         modeimg.setAttribute('src', 'darkmode.svg');
         currentmode = 0;
     }
