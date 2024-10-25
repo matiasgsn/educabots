44f42c30ebcbc01160f958bdd27da58385be179b
Matias <matiasgsn@gmail.com>
2024-10-25 17:40:26 -0300

    Batch de melhorias de design + imagens nos cards

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function swapMode(){
         document.documentElement.style.setProperty('--main-color-filter', 'var(--dark-color-filter)');
         document.documentElement.style.setProperty('--opposite-color-filter', 'var(--light-color-filter)');
 
+        document.documentElement.style.setProperty('--logo-color-filter', 'brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7500%) hue-rotate(264deg) brightness(109%) contrast(109%)')
+
         modeimg.setAttribute('src', 'images/lightmode.svg');
         currentmode = 1;
     } else {
@@ -14,6 +16,8 @@ function swapMode(){
         document.documentElement.style.setProperty('--opposite-color', 'var(--dark-color)');
         document.documentElement.style.setProperty('--main-color-filter', 'var(--light-color-filter)');
         document.documentElement.style.setProperty('--opposite-color-filter', 'var(--dark-color-filter)');
+
+        document.documentElement.style.setProperty('--logo-color-filter', 'none')
 
         modeimg.setAttribute('src', 'images/darkmode.svg');
         currentmode = 0;
